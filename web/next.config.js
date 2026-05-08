219771e3c65a7dcf85dddbdb0d7ca7013fc8da44
“Vinicius <viniciusldacal@gmail.com>
2026-05-08 15:28:55 +0900

    Modify next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  // output: 'standalone',
   transpilePackages: ['@pet360/shared'],
   images: {
     remotePatterns: [
